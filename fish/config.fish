2d51cd1990f00a9eb8bccd8af22cc98f263ec9cb
Garrett Ladley <garrett.ladley@klaviyo.com>
2026-08-05 13:25:23 -0400

    feat(fish): configure homebrew environment

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 fish_add_path "$HOME/bin" "$HOME/.local/bin"
 
+# Make recognized commands, functions, and aliases distinct from plain text.
+set -g fish_color_command blue
+
+# Homebrew environment (PATH, MANPATH, INFOPATH)
+env SHELL=/opt/homebrew/bin/fish /opt/homebrew/bin/brew shellenv | source
+
 if test -f "$HOME/.config/fish/secrets.fish"
     source "$HOME/.config/fish/secrets.fish"
 end
