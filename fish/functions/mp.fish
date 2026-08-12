2c1c0f66c9016b13d664c991764f868fe38fe83d
Garrett Ladley <garrett.ladley@klaviyo.com>
2026-08-12 16:37:37 -0400

    fix(fish): resolve mp default branch from origin

--- fish/functions/mp.fish
+++ fish/functions/mp.fish
@@ -1,4 +1,10 @@
-function mp --description 'Check out main and pull'
-    m
+function mp --description 'Check out the remote default branch and pull'
+    set -l default_branch (git symbolic-ref --quiet --short refs/remotes/origin/HEAD)
+    or begin
+        echo "mp: unable to determine origin's default branch; run 'git remote set-head origin --auto'." >&2
+        return 1
+    end
+
+    git checkout (string replace 'origin/' '' -- $default_branch)
     and pul $argv
 end
