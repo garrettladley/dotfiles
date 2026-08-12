247fe5d5c6109fc78c6d10641c4437329b47b193
Garrett Ladley <garrett.ladley@klaviyo.com>
2026-08-12 16:39:32 -0400

    fix(fish): resolve m from remote default branch

--- fish/functions/m.fish
+++ fish/functions/m.fish
@@ -1,3 +1,9 @@
-function m --wraps='git checkout main' --description 'Check out main'
-    git checkout main $argv
+function m --description 'Check out the remote default branch'
+    set -l default_branch (git symbolic-ref --quiet --short refs/remotes/origin/HEAD)
+    or begin
+        echo "m: unable to determine origin's default branch; run 'git remote set-head origin --auto'." >&2
+        return 1
+    end
+
+    git checkout (string replace 'origin/' '' -- $default_branch) $argv
 end
