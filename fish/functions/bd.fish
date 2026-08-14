76132661b5ef7b04881373a1a8050fdc4ff58fae
Garrett Ladley <garrett.ladley@klaviyo.com>
2026-08-14 10:10:18 -0400

    fix(fish): resolve diff base branch from origin

--- fish/functions/bd.fish
+++ fish/functions/bd.fish
@@ -1,4 +1,10 @@
 function bd --description 'Show the merge-base diff with bat'
-    env -u GIT_EXTERNAL_DIFF git diff (git merge-base origin/main HEAD) $argv |
+    set -l default_branch (git symbolic-ref --quiet --short refs/remotes/origin/HEAD)
+    or begin
+        echo "bd: unable to determine origin's default branch; run 'git remote set-head origin --auto'." >&2
+        return 1
+    end
+
+    env -u GIT_EXTERNAL_DIFF git diff (git merge-base $default_branch HEAD) $argv |
         bat --language=diff --paging=never
 end
