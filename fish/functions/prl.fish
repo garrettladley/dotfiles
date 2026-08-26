1a90f55c7ebc66307531dbd270314f4430560ca5
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2026-08-26 16:06:08 -0400

    fix(fish): suppress prl command noise (#11)

--- fish/functions/prl.fish
+++ fish/functions/prl.fish
@@ -1,3 +1,5 @@
 function prl --description 'Print the current pull request URL'
-    gh pr view --json url --jq .url $argv
+    set -lx GH_NO_UPDATE_NOTIFIER 1
+    gh pr view --json url --jq .url $argv 2>| command cat >&2
+    return $pipestatus[1]
 end
