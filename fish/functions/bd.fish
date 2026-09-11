1c7556356114d5ec5a0221e52b17eac358e0c719
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2026-09-10 21:48:24 -0400

    fix(fish): preserve colors in diff helpers (#16)

--- fish/functions/bd.fish
+++ fish/functions/bd.fish
@@ -1,4 +1,8 @@
 function bd --description 'Show the merge-base diff with bat'
+    set -l GIT_PAGER
+    set -l PAGER
+    set -l NO_COLOR
+
     set -l default_branch (git symbolic-ref --quiet --short refs/remotes/origin/HEAD)
     or begin
         echo "bd: unable to determine origin's default branch; run 'git remote set-head origin --auto'." >&2
