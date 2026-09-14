7f78075c6aee4b4a4c3e69dd1bacb1317944c935
Garrett Ladley <garrett.ladley@klaviyo.com>
2026-09-14 15:16:09 -0400

    feat(fish): add codex wrappers

---
+++ fish/functions/xluna.fish
@@ -0,0 +1,8 @@
+function xluna --wraps=codex --description 'Run Codex with Luna and xhigh reasoning effort'
+    set -l model gpt-5.6-luna
+    set -l reasoning_effort xhigh
+    set -l NO_COLOR
+
+    command codex --model "$model" \
+        --config "model_reasoning_effort=\"$reasoning_effort\"" $argv
+end
